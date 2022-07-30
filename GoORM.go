22541c106dcdcd49f218b60ad8e3c65d34a20021
JesonSun <Jeson_sun@163.com>
2022-07-30 22:20:52 +0800

    对象表结构映射

---
+++ GoORM.go
@@ -0,0 +1,45 @@
+package GoORM
+
+import (
+	"GoORM/dialect"
+	"GoORM/log"
+	"GoORM/session"
+	"database/sql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type Engine struct {
+	db      *sql.DB
+	dialect dialect.Dialect
+}
+
+func NewEngine(driver string, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	dial, ok := dialect.GetDialect(driver)
+	if !ok {
+		log.Errorf("dialect %s Not Found", driver)
+		return
+	}
+	e = &Engine{db: db, dialect: dial}
+	log.Info("Connect database success")
+	return
+}
+
+func (engine *Engine) Close() {
+	if err := engine.db.Close(); err != nil {
+		log.Error("Failed to close database")
+	}
+	log.Info("Close database success")
+}
+
+func (engine *Engine) NewSession() *session.Session {
+	return session.New(engine.db, engine.dialect)
+}
